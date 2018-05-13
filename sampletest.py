b2b0af1cda605fd876ca5723885193b09c1921c1
VaniSHadow <550185145@qq.com>
2018-05-13 11:23:16 +0800

    Create sampletest.py
    使用样例

---
+++ sampletest.py
@@ -0,0 +1,8 @@
+#! -*- coding: utf8 -*-
+
+from Graph import *
+from TPGenerator import *
+
+g = 'DAG'
+gener = TPGenerator()
+gener.GenerateTP("C:\\Users\\admin\\Documents\\CODINGTANG\\新建文件夹\\test", g)

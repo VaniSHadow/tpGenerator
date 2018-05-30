ed9bcceaeaa254b5ceb8a11d34d94c96eb23ef7d
VaniSHadow <550185145@qq.com>
2018-05-30 16:30:13 +0800

    Update sampletest.py

--- sampletest.py
+++ sampletest.py
@@ -4,5 +4,8 @@ from Graph import *
 from TPGenerator import *
 
 g = 'DAG'
+path = r"D:\Workspace\python3\tpGenerator\test"
+file_name = "test"
+exe_name = "ttest.exe"
 gener = TPGenerator()
-gener.GenerateTP("C:\\Users\\admin\\Documents\\CODINGTANG\\新建文件夹\\test", g)
+gener.GenerateTP(path, file_name, exe_name, g, node_num=100, edge_num=500)
